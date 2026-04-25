750dc2af82aa833866d60b832bbc3ad33d62de89
Keith Thomson <camelbyte@proton.me>
2026-04-24 22:07:46 -0400

    add refs

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
+# Welcome!
+
+# Using Python 3.14.4
+
+
+
+# Define a function.
+def say_hi(name: str) -> str:
+    """ A function that takes a string (name), and return a greeting. """
+    return f"Hi, {name}!"
+
+
+# The 'main' function (the program) run below
 def main():
-    print("Hello from learnpython!")
+    print("Welcome to LearnPython!")
+    print("This is a new line.")
+    print(say_hi("Thucydides")) # This is a comment
+    print(say_hi("Plato"))
+    print(say_hi("Aristotle"))
+    print(say_hi("Socrates"))
 
 
+# This runs the 'main' function above.
 if __name__ == "__main__":
     main()
